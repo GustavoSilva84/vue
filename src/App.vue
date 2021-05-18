e5e6e9a1474871e311b4ce3d421d5d75057a2233
Gustavo <72147773+Gustavo-Silva-01@users.noreply.github.com>
2021-05-18 16:45:27 -0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+
+    <HelloWorld msg="ss"/>
+    <Teste/>
+
+  </div>
+</template>
+
+<script>
+
+import Teste from './components/rr.vue'
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    Teste
+  }
+}
+
+</script>
+
+<style>
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+</style>/
---
+++ src/components/rr.vue
@@ -0,0 +1,51 @@
+<template>
+    
+    <form v-on:submit="enviar(event)">
+
+        <input type="text" v-model="msg">
+        <button type="submit"> Enviar </button>
+        <li v-for="dados in chat" v-bind:key="dados.msg">
+            {{ dados.msg }}
+        </li>
+
+    </form>
+
+</template>
+
+
+<script>
+
+export default {
+
+    name: 'Teste',
+ 
+    data() {
+
+        return {
+            msg: '',
+            chat: []
+        }
+
+    },
+    
+    methods: {
+
+        enviar(event) {
+            event.pre
+            if(this.msg.trim() == '') return;
+            this.chat.push({msg: this.msg});
+        }
+
+    },
+
+    computed: {
+
+        mostraMsg() {
+            return this.chat;
+        }
+
+    }
+
+}
+
+</script>
